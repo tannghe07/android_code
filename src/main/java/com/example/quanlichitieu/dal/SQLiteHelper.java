408a127b7a126cae6772afc5e8e558254fcb0771
tannghe07 <tannghe07@gmail.com>
2023-04-27 11:10:11 +0700

    new commit

---
+++ src/main/java/com/example/quanlichitieu/dal/SQLiteHelper.java
@@ -0,0 +1,152 @@
+package com.example.quanlichitieu.dal;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.quanlichitieu.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.Nullable;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "Chitieu.db";
+    private static int DATABASE_VERSION = 1;
+    public SQLiteHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE items(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "title TEXT, category TEXT, price TEXT, date TEXT)";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+    }
+
+    public List<Item> getALL(){
+        List<Item> list = new ArrayList<>();
+        SQLiteDatabase st = getReadableDatabase();
+        String order = "date DESC";
+        Cursor rs = st.query("items", null, null, null, null, null, order);
+        while(rs!=null && rs.moveToNext()){
+            int id = rs.getInt(0);
+            String title = rs.getString(1);
+            String category = rs.getString(2);
+            String price = rs.getString(3);
+            String date = rs.getString(4);
+            list.add(new Item(id, title, category, price, date));
+        }
+        return list;
+    }
+
+    public long addItem(Item i){
+        ContentValues values = new ContentValues();
+        values.put("title", i.getTitle());
+        values.put("category", i.getCategory());
+        values.put("price", i.getPrice());
+        values.put("date", i.getDate());
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        return sqLiteDatabase.insert("items", null, values);
+    }
+
+    public List<Item> getByDate(String date){
+        List<Item> list = new ArrayList<>();
+        String whereClause = "date like ?";
+        String [] whereArgs = {date};
+        SQLiteDatabase st = getReadableDatabase();
+        Cursor rs = st.query("items", null, whereClause, whereArgs, null, null, null);
+        while(rs!=null && rs.moveToNext()){
+            int id = rs.getInt(0);
+            String title = rs.getString(1);
+            String category = rs.getString(2);
+            String price = rs.getString(3);
+            list.add(new Item(id, title, category, price, date));
+        }
+        return list;
+    }
+
+    public int update(Item i){
+        ContentValues values = new ContentValues();
+        values.put("title", i.getTitle());
+        values.put("category", i.getCategory());
+        values.put("price", i.getPrice());
+        values.put("date", i.getDate());
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        String whereClause = "id = ?";
+        String [] whereArgs = {Integer.toString(i.getId())};
+        return sqLiteDatabase.update("items", values, whereClause, whereArgs);
+    }
+
+    public int delete(int id){
+        String whereClause = "id = ?";
+        String [] whereArgs = {Integer.toString(id)};
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        return sqLiteDatabase.delete("items", whereClause, whereArgs);
+    }
+
+    public List<Item> searchByTitle(String key){
+        List<Item> list = new ArrayList<>();
+        String whereClause = "title like ?";
+        String [] whereArgs = {"%"+key+"%"};
+        SQLiteDatabase st = getReadableDatabase();
+        Cursor rs = st.query("items", null, whereClause, whereArgs, null, null, null);
+        while(rs!=null && rs.moveToNext()){
+            int id = rs.getInt(0);
+            String title = rs.getString(1);
+            String category = rs.getString(2);
+            String price = rs.getString(3);
+            String date = rs.getString(4);
+            list.add(new Item(id, title, category, price, date));
+        }
+        return list;
+    }
+
+    public List<Item> searchByCategory(String category){
+        List<Item> list = new ArrayList<>();
+        String whereClause = "category like ?";
+        String [] whereArgs = {category};
+        SQLiteDatabase st = getReadableDatabase();
+        Cursor rs = st.query("items", null, whereClause, whereArgs, null, null, null);
+        while(rs!=null && rs.moveToNext()){
+            int id = rs.getInt(0);
+            String title = rs.getString(1);
+            String c = rs.getString(2);
+            String price = rs.getString(3);
+            String date = rs.getString(4);
+            list.add(new Item(id, title, c, price, date));
+        }
+        return list;
+    }
+
+    public List<Item> searchByDateFromTo(String from, String to){
+        List<Item> list = new ArrayList<>();
+        String whereClause = "date BETWEEN ? AND ?";
+        String [] whereArgs = {from.trim(), to.trim()};
+        SQLiteDatabase st = getReadableDatabase();
+        Cursor rs = st.query("items", null, whereClause, whereArgs, null, null, null);
+        while(rs!=null && rs.moveToNext()){
+            int id = rs.getInt(0);
+            String title = rs.getString(1);
+            String category = rs.getString(2);
+            String price = rs.getString(3);
+            String date = rs.getString(4);
+            list.add(new Item(id, title, category, price, date));
+        }
+        return list;
+    }
+}
---
+++ src/main/java/com/example/quanlichitieu/fragment/FragmentSearch.java
@@ -0,0 +1,172 @@
+package com.example.quanlichitieu.fragment;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.quanlichitieu.AddActivity;
+import com.example.quanlichitieu.R;
+import com.example.quanlichitieu.adapter.RecycleViewAdapter;
+import com.example.quanlichitieu.dal.SQLiteHelper;
+import com.example.quanlichitieu.model.Item;
+
+import java.util.Calendar;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FragmentSearch extends Fragment implements View.OnClickListener{
+    private RecyclerView recyclerView;
+    private TextView tvTong;
+    private Button btSearch;
+    private SearchView searchView;
+    private EditText eFrom, eTo;
+    private Spinner category;
+    private RecycleViewAdapter adapter;
+    private SQLiteHelper db;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+        adapter = new RecycleViewAdapter();
+        db = new SQLiteHelper(getContext());
+        List<Item> list = db.getALL();
+        adapter.setList(list);
+        tvTong.setText("tong: "+tong(list));
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                List<Item> list = db.searchByTitle(s);
+                tvTong.setText("tong: "+tong(list));
+                adapter.setList(list);
+                return true;
+            }
+        });
+        eFrom.setOnClickListener(this);
+        eTo.setOnClickListener(this);
+        btSearch.setOnClickListener(this);
+        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                String c = category.getItemAtPosition(position).toString();
+                List<Item> list;
+                if(!c.equalsIgnoreCase("all")){
+                    list = db.searchByCategory(c);
+                }else {
+                    list = db.getALL();
+                }
+                adapter.setList(list);
+                tvTong.setText("tong: "+tong(list));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private int tong(List<Item> list){
+        int t = 0;
+        for(Item i:list){
+            t+=Integer.parseInt(i.getPrice());
+        }
+        return t;
+    }
+
+    private void initView(View view) {
+        recyclerView = view.findViewById(R.id.recycleView);
+        tvTong = view.findViewById(R.id.tvTong);
+        btSearch = view.findViewById(R.id.btSearch);
+        searchView = view.findViewById(R.id.search);
+        eFrom = view.findViewById(R.id.eFrom);
+        eTo = view.findViewById(R.id.eTo);
+        category = view.findViewById(R.id.spCategory);
+        String[] arr = getResources().getStringArray(R.array.category);
+        String[] arr1 = new String[arr.length+1];
+        arr1[0] = "All";
+        for(int i=0;i<arr.length;i++){
+            arr1[i+1] = arr[i];
+        }
+        category.setAdapter(new ArrayAdapter<String>(getContext(), R.layout.item_spinner, arr1));
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==eFrom){
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                    String date = "";
+                    if(m>8){
+                        date = d+"/"+(m+1)+"/"+y;
+                    }else {
+                        date = d+"/0"+(m+1)+"/"+y;
+                    }
+                    eFrom.setText(date);
+                }
+            }, year, month, day);
+            dialog.show();
+        }
+        if(view==eTo){
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                    String date = "";
+                    if(m>8){
+                        date = d+"/"+(m+1)+"/"+y;
+                    }else {
+                        date = d+"/0"+(m+1)+"/"+y;
+                    }
+                    eTo.setText(date);
+                }
+            }, year, month, day);
+            dialog.show();
+        }
+        if(view==btSearch){
+            String from = eFrom.getText().toString();
+            String to = eTo.getText().toString();
+            if(!from.isEmpty() && !to.isEmpty()){
+                List<Item> list = db.searchByDateFromTo(from, to);
+                adapter.setList(list);
+                tvTong.setText("tong: "+tong(list));
+            }
+        }
+    }
+}
